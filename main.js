80ad848f6011a18bf1b79394373161eaf7334185
Atayi <atayisika@gmail.com>
2019-10-02 11:33:19 +0200

    Modification des couleurs du script

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
  
-console.log(chalk.white('Hadibéré'));
-console.log(chalk.red('Amel'));
+console.log(chalk.grey('Hadibéré'));
+console.log(chalk.yellow('Amel'));
 console.log(chalk.green('Salim'));
-console.log(chalk.yellow('Sika'));
+console.log(chalk.red('Sika'));
